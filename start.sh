db9fa512dfb380200781e5d9a3a3e08c3f476bca
El tío judai <alexnarcista@gmail.com>
2025-08-20 16:20:05 -0600

    Start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
-#!bin/bash
+#!/bin/bash
+
 GREEN='\033[0;32m'
+RESET='\033[0m'
+
+echo -e "${GREEN}🚀 Iniciando Empire Bot v1.0...${RESET}"
+sleep 1
+
 while :
 do
-echo ""
-    npm start
-    sleep 1
-
-done 
+  npm start
+  sleep 1
+done
