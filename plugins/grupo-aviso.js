d4fd2af6bcdd56e30c74b8d77eb39f2ee7373deb
El tío judai <alexnarcista@gmail.com>
2025-08-24 21:40:38 -0600

    grupo-aviso.js

--- plugins/grupo-aviso.js
+++ plugins/grupo-aviso.js
@@ -1,11 +1,14 @@
 let handler = async (m, { conn }) => {
-  let text = m.text.slice(6).trim()
+  let text = m.text.slice(6).trim() // asumiendo que tu comando empieza con "aviso "
   if (!text) return
 
   let participants = (await conn.groupMetadata(m.chat)).participants.map(p => p.id)
 
-  conn.sendMessage(m.chat, {
-    text,
+  // Caracteres invisibles para ocultar menciones y evitar "leer más"
+  const invisible = String.fromCharCode(8206).repeat(850)
+
+  await conn.sendMessage(m.chat, {
+    text: `${invisible}\n${text}`,
     mentions: participants
   }, { quoted: m })
 }
